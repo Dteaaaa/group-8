86598b662a0ad739435c4cbe3243199b4e219609
Dteaaaa <155054478+Dteaaaa@users.noreply.github.com>
2023-12-28 14:26:03 +0700

    bruh

---
+++ script/script.js
@@ -0,0 +1,12 @@
+function gotoViet() {
+    window.location.href = 'viet.html';
+}
+function gotoChien() {
+    window.location.href = 'chien.html';
+}
+function gotoThinh() {
+    window.location.href = 'thinh.html';
+}
+function gotoMain() {
+    window.location.href = 'index.html';
+}
